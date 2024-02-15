c4e37ab3c3b8a37617d425a9d25f89d090f3a811
HR. Lee <matt14507@gmail.com>
2024-02-15 18:09:32 +0900

    fest: 전역 로딩 애니메이션 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 tailwind.config = {
   theme: {
     extend: {
+      fontFamily: {
+        happiness: ["HappinessSans", "sans-serif"],
+        Nanum: ["NanumSquare", "serif"],
+      },
       colors: {
         clifford: "#da373d",
       },
